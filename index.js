44571a8c1de25f4ffd05872cc71e09d8783f491d
Rose <r.d.jeanlouis01@gmail.com>
2021-03-28 12:29:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const body =document.body
+let newHeader = document.createElement("h1");
+newHeader.id ='victory';
+newHeader.innerHTML = "Rose is the champion";
+newHeader.append(h1);
